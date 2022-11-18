6e1160c9dbc9f591f6d0815319fb0a317151db7f
wyylame <w1llhorn@yandex.ru>
2022-11-18 18:07:09 +0300

    fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -12,18 +12,16 @@ const Header = (props) => {
   return (
     <header className="header">
       <div className="header__top">
-        <div
-          ref={burgerRef}
-          className="burger-menu__button"
-          onClick={handleClickBurgerBtn}
-        >
+        <div ref={burgerRef} className="burger-menu__button" onClick={handleClickBurgerBtn}>
           <span></span>
         </div>
         <img src="./assets/img/logo.png" alt="" className="logo" />
       </div>
       <main className="header__main">
         <div className="header__main-bg"></div>
-        <h1 className="title">ПРОЕКТ «ПРАВОВОЙ МИР КАВКАЗА»</h1>
+        <h1 className="title">
+          ПРОЕКТ <br /> «ПРАВОВОЙ МИР КАВКАЗА»
+        </h1>
       </main>
     </header>
   );
